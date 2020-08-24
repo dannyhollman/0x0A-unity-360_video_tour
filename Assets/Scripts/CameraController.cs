c1202a9d36d805eae634df40fd3d5614c9a5cedc
Danny Hollman <dannyhollman@gmail.com>
2020-08-23 23:48:10 -0400

    [INIT] Add all files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject livingRoom;
+	public GameObject cube;
+	public GameObject mezz;
+	public GameObject cantina;
+	public Animator animator;
+	private GameObject current;
+
+	void Start()
+	{
+		current = livingRoom;
+	}
+
+	public void LivingRoom()
+	{
+		StartCoroutine(WaitForSeconds());
+		animator.SetTrigger("transition");
+		current.SetActive(false);
+		livingRoom.SetActive(true);
+		current = livingRoom;
+	}
+
+	public void Cube()
+	{
+		StartCoroutine(WaitForSeconds());
+		animator.SetTrigger("transition");
+		current.SetActive(false);
+		cube.SetActive(true);
+		current = cube;
+	}
+
+	public void Mezzanine()
+	{
+		StartCoroutine(WaitForSeconds());
+		animator.SetTrigger("transition");
+		current.SetActive(false);
+		mezz.SetActive(true);
+		current = mezz;
+	}
+
+	public void Cantina()
+	{
+		StartCoroutine(WaitForSeconds());
+		animator.SetTrigger("transition");
+		current.SetActive(false);
+		cantina.SetActive(true);
+		current = cantina;
+	}
+
+	IEnumerator WaitForSeconds()
+	{
+		yield return new WaitForSeconds(3);
+		Debug.Log("test");
+	}
+}
---
+++ Assets/Scripts/infoBoxController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infoBoxController : MonoBehaviour
+{
+	public GameObject infoBox;
+
+	public void InfoBox()
+	{
+		if (infoBox.activeInHierarchy)
+			infoBox.SetActive(false);
+		else
+			infoBox.SetActive(true);
+	}
+}
